18cebd6cdab63b10a89805c20ed96fcd3ff1e0a9
prs2rnn <developer.prs2rnn@gmail.com>
2026-05-18 14:26:31 +0500

    refactor: telegram

--- src/mycardbot/app.py
+++ src/mycardbot/app.py
@@ -3,14 +3,14 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
-from core.commands import set_commands
 from core.config import setting
 from core.database import bot_db
-from core.routers import setup_router
 from core.setup_logging import setup_logger, setup_telegram_logger
 from middlewares.check_ban import CheckUserIsBanned
 from middlewares.logging import LoggingMiddleware
 from services.tasks import cleanup_task
+from telegram.commands import set_commands
+from telegram.routers import setup_router
 
 
 async def main():
